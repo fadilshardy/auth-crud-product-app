376c1bbed3431b03ac2ca65eaf5309bf575fa5ba
fadilshardy <fadilshrdy@gmail.com>
2023-01-01 17:48:04 +0700

    build: configure NextAuth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,58 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import * as AuthApi from '../../../src/lib/authApi';
+export default NextAuth({
+    providers: [
+        CredentialsProvider({
+            id: "login",
+            async authorize(credentials) {
+                const payload = {
+                    email: credentials.email,
+                    password: credentials.password,
+                };
+                try {
+                    return await AuthApi.login(payload);
+                } catch (error) {
+                    throw new Error(error);
+                }
+            },
+        }),
+
+    ],
+    pages: {
+        signIn: '/login',
+    },
+    session: {
+        jwt: true,
+        maxAge: 30 * 24 * 60 * 60,
+    },
+    events: {
+        async signOut({ token, session }) {
+            try {
+                return await AuthApi.logout();
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.user = user;
+                return {
+                    ...token,
+                    accessToken: user.data.token,
+                    refreshToken: user.data.refreshToken,
+                };
+            }
+            return token;
+
+        },
+
+        async session({ session, token }) {
+            session.user = token.user.data;
+            return session;
+        },
+    },
+    secret: process.env.JWT_SECRET
+});
